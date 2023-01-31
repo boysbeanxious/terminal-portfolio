81e3f1532337bf7f7ed5e6c7de70c4f76da9e34a
eenimeeniminim0 <ccunyday@naver.com>
2023-01-31 12:58:52 +0900

    Update ItemsList.js

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -170,10 +170,13 @@ export default function ItemsList() {
           width: "70%",
         }}
       >
-        {`lrwxr-xr-x 1 kielx admin ${item.node.frontmatter.added} ${item.node.frontmatter.slug} -> `}
-        <a href={item.node.frontmatter.popupGithubLink}>
-          {item.node.frontmatter.popupGithubLink}
-        </a>
+        {`lrwxr-xr-x 1 kielx admin ${item.node.frontmatter.added} ${item.node.frontmatter.slug} `}
+	${item.node.frontmatter.popupGithubLink}==false? (""):(
+		<a href={item.node.frontmatter.popupGithubLink}>
+		  {item.node.frontmatter.popupGithubLink}
+		</a>
+	)
+        
       </span>
     </li>
   ))
